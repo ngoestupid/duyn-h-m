da368b622301513176a04251d164b1b08a14fa66
ngoestupid <85279776+ngoestupid@users.noreply.github.com>
2022-11-07 23:50:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Nào nào chúng ta có gì nào",
-    introTitle: 'Gửi cho Duyn Hâm',
+    introTitle: 'Duyn Hâm',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
